f3aeb82524946f13f48d050e7d07a1f751101922
Glitch (beautiful-thirsty-magic) <none>
2020-05-24 22:21:15 +0000

    🏔☕️ Checkpoint ./routes.js:907439/2210 ./server.js:907439/3247 ./auth.js:907439/1349

--- server.js
+++ server.js
@@ -3,31 +3,20 @@ const routes      = require('./routes.js');
 const auth        = require('./auth.js');
 
 const express     = require("express");
-const fccTesting  = require("./freeCodeCamp/fcctesting.js");
 const session     = require('express-session');
 const passport    = require('passport');
 const mongo       = require('mongodb').MongoClient;
-const ObjectID    = require('mongodb').ObjectID;
-const bcrypt      = require('bcrypt');
+
 
 const LocalStrategy = require('passport-local');
 
 const app = express();
 
-fccTesting(app); //For FCC testing purposes
 app.use("/public", express.static(process.cwd() + "/public"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 app.set('view engine', 'pug')
-
-app.use(session({
-  secret: process.env.SESSION_SECRET,
-  resave: true,
-  saveUninitialized: true,
-}));
-app.use(passport.initialize());
-app.use(passport.session());
 
 
 mongo.connect(process.env.DATABASE,{ useUnifiedTopology: true }, (err, db) => {
@@ -40,94 +29,6 @@ mongo.connect(process.env.DATABASE,{ useUnifiedTopology: true }, (err, db) => {
       
         routes(app, db);
 
-        passport.serializeUser((user, done) => {
-          done(null, user._id);
-        });
-
-        passport.deserializeUser( (id, done) => {
-            db.db('test').collection('users').findOne(
-                {_id: new ObjectID(id)},
-                (err, doc) => {
-                    done(null, doc);
-                }
-            );
-        });
-      
-        passport.use(new LocalStrategy(
-          function(username, password, done) {
-            db.db('test').collection('users').findOne({ username: username }, function (err, user) {
-              console.log('User '+ username +' attempted to log in.');
-              if (err) { return done(err); }
-              if (!user) { return done(null, false); }
-              if (!bcrypt.compareSync(password, user.password)) { return done(null, false); }
-              return done(null, user);
-            });
-          }
-        ));
-      
-        function ensureAuthenticated(req, res, next) {
-          if (req.isAuthenticated()) {
-              return next();
-          }
-          res.redirect('/');
-        };
-
-        app.route('/')
-          .get((req, res) => {
-            res.render(process.cwd() + '/views/pug/index', {title: 'Home Page', message: 'Please login', showLogin: true, showRegistration: true});
-          });
-      
-        app.route('/register')
-        .post((req, res, next) => {
-          var hash = bcrypt.hashSync(req.body.password, 8);
-          db.db('test').collection('users').findOne({ username: req.body.username }, function(err, user) {
-            if (err) {
-              next(err);
-            } else if (user) {
-              res.redirect('/');
-            } else {
-              db.db('test').collection('users').insertOne({
-                username: req.body.username,
-                password: hash
-              },
-                (err, doc) => {
-                  if (err) {
-                    res.redirect('/');
-                  } else {
-                    next(null, user);
-                  }
-                }
-              )
-            }
-          })
-        },
-          passport.authenticate('local', { failureRedirect: '/' }),
-          (req, res, next) => {
-            res.redirect('/profile');
-          }
-        );
-      
-        app.route('/login')
-          .post(passport.authenticate('local', { failureRedirect: '/' }),(req,res) => {
-               res.redirect('/profile');
-          });
-          
-        app.route('/logout')
-          .get((req, res) => {
-            req.logout();
-            res.redirect('/');
-        });
-      
-        app.route('/profile')
-         .get(ensureAuthenticated, (req,res) => {
-            res.render(process.cwd() + '/views/pug/profile', {username: req.user.username});
-         });
-      
-        app.use((req, res, next) => {
-          res.status(404)
-            .type('text')
-            .send('Not Found');
-        });
 
         app.listen(process.env.PORT || 3000, () => {
           console.log("Listening on port " + process.env.PORT);
